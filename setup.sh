553c60bd275ba881d86e8afedaf93ad3de73c56b
xLasercut <tianxun.wang2004@gmail.com>
2022-04-27 10:37:34 +0100

    added local and programs

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 HOME_DIR=$HOME
-REPO_DIR="$HOME/git/i3-kde"
+REPO_DIR="$HOME_DIR/git/i3-kde"
 REPO_HOME_DIR="$REPO_DIR/home"
 
-function _create_dir_if_not_exist() {
+function _create_dir() {
     local dir_path=$1
     if [[ -d $dir_path ]]; then 
         rm -rf $dir_path 
@@ -12,11 +12,36 @@ function _create_dir_if_not_exist() {
     mkdir -p $dir_path
 }
 
+function _install_packages() {
+    sudo pacman -S i3-gaps rofi picom feh
+}
+
 function _setup_i3() {
-    local i3_dir="$HOME/.config/i3"
-    _create_dir_if_not_exist $i3_dir
+    local i3_dir="$HOME_DIR/.config/i3"
+    _create_dir $i3_dir
 
     ln -s "$REPO_HOME_DIR/.config/i3/config" "$i3_dir/config"
 }
 
+function _setup_fonts() {
+    local fonts_dir="$HOME_DIR/.fonts"
+    cp -r "$REPO_HOME_DIR/.fonts/." $fonts_dir
+}
+
+function _setup_local() {
+    local locals_dir="$HOME_DIR/.local"
+    cp -r "$REPO_HOME_DIR/.local/" $locals_dir
+}
+
+function _setup_antigen() {
+    local antigen_dir="$HOME_DIR/antigen"
+    if [[ -d $antigen_dir ]]; then 
+        rm -rf $antigen_dir 
+    fi
+    git clone git@github.com:zsh-users/antigen.git $antigen_dir
+}
+
 _setup_i3
+_setup_fonts
+_setup_local
+_setup_antigen
