b8e5231444bc0435a0f85f6584897400501eef5c
xLasercut <tianxun.wang2004@gmail.com>
2022-05-06 13:35:52 +0100

    now uses linux home config as submodule

--- home/.local/share/konsole/Shell.profile
+++
@@ -1,9 +0,0 @@
-[Appearance]
-ColorScheme=Misterioso
-Font=Fira Mono for Powerline,12,-1,5,50,0,0,0,0,0
-UseFontLineChararacters=false
-
-[General]
-Command=/bin/zsh
-Name=Shell
-Parent=FALLBACK/
--- setup.sh
+++ setup.sh
@@ -1,79 +1,27 @@
 #!/bin/bash
 
-HOME_DIR=$HOME
-REPO_DIR="$HOME_DIR/git/i3-kde"
-REPO_HOME_DIR="$REPO_DIR/home"
-REPO_USR_DIR="$REPO_DIR/usr"
+export ADD_EXECUTABLE=1
+export ADD_VM_FIXES=1
+export ADD_LOCALS=1
+export ADD_THEMES=1
+export ADD_FONTS=1
 
-function _create_dir() {
-    local dir_path=$1
-    if [[ -d $dir_path ]]; then 
-        rm -rf $dir_path 
-    fi
-    mkdir -p $dir_path
-}
+# setup submodules
+git submodule init
+git submodule update
 
-function _install_packages() {
-    sudo pacman -S i3-gaps rofi feh picom
-}
+# setup home config
+pushd ./linux-home-config
+./setup.sh
+popd
 
-function _setup_i3() {
-    local i3_dir="$HOME_DIR/.config/i3"
-    _create_dir $i3_dir
+# set kde max windows no border
+echo "[Windows]" >> ~/.config/kwinrc
+echo "HighlightWindows=false" >> ~/.config/kwinrc
 
-    ln -s "$REPO_HOME_DIR/.config/i3/config" "$i3_dir/config"
-}
+# update system
+sudo pacman -Syyu
+sudo pacman -S vim plasma5-applets-window-buttons gufw
 
-function _setup_picom() {
-    local picom_dir="$HOME_DIR/.config/picom"
-    _create_dir $picom_dir
-
-    ln -s "$REPO_HOME_DIR/.config/picom/picom.conf" "$picom_dir/picom.conf"
-}
-
-function _setup_rofi() {
-    local rofi_dir="$HOME_DIR/.config/rofi" 
-    _create_dir $rofi_dir
-
-    ln -s "$REPO_HOME_DIR/.config/rofi/config.rasi" "$rofi_dir/config.rasi" 
-    ln -s "$REPO_HOME_DIR/.config/rofi/nord.rasi" "$rofi_dir/nord.rasi" 
-}
-
-function _setup_fonts() {
-    cp -r "$REPO_HOME_DIR/.fonts/." "$HOME_DIR/.fonts"
-}
-
-function _setup_local() {
-    cp -r "$REPO_HOME_DIR/.local/." "$HOME_DIR/.local"
-}
-
-function _setup_antigen() {
-    local antigen_dir="$HOME_DIR/antigen"
-    if [[ -d $antigen_dir ]]; then 
-        rm -rf $antigen_dir 
-    fi
-    git clone https://github.com/zsh-users/antigen.git $antigen_dir
-}
-
-function _setup_desktopsession() {
-    sudo cp -r "$REPO_USR_DIR/." "/usr"
-}
-
-function _setup_home_dots() {
-    cp "$REPO_HOME_DIR/.imwheelrc" $HOME_DIR
-}
-
-function _setup_programs() {
-    cp -r "$REPO_HOME_DIR/programs/." "$HOME_DIR/programs/"
-}
-
-_install_packages
-_setup_desktopsession
-_setup_i3
-_setup_picom
-_setup_fonts
-_setup_local
-_setup_antigen
-_setup_home_dots
-_setup_programs
-_setup_rofi
+# change shell
+chsh -s /bin/zsh
